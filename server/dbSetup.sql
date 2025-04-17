53bcb7b517d540f6a7783d74327fef22bbea8cd2
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-17 09:39:06 -0600

    albums table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -6,3 +6,18 @@ CREATE TABLE IF NOT EXISTS accounts(
   email VARCHAR(255) UNIQUE COMMENT 'User Email',
   picture VARCHAR(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE albums(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  title TINYTEXT NOT NULL,
+  description TEXT,
+  cover_img TEXT NOT NULL,
+  archived BOOLEAN NOT NULL,
+  category ENUM('aesthetics', 'games', 'animals', 'food', 'vibes', 'misc'),
+  creator_id VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE
+);
+
+DROP TABLE albums;
