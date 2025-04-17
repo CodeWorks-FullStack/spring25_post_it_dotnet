b6c1435e0b36f3aa272c0ac9ed20063e6f8866ea
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-17 11:29:40 -0600

    added pictures files

---
+++ server/Services/PicturesService.cs
@@ -0,0 +1,11 @@
+namespace post_it_dotnet.Services;
+
+public class PicturesService
+{
+  public PicturesService(PicturesRepository repository)
+  {
+    _repository = repository;
+  }
+  private readonly PicturesRepository _repository;
+
+}
